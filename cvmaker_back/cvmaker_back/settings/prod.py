82da8bd8547f8b6fd7d3032fc443f1847275e69e
Taras Stasyuk <taras-st@10.211.97.216-vpn.dhcp.yndx.net>
2025-04-01 10:14:25 +0300

    fix

--- cvmaker_back/cvmaker_back/settings/prod.py
+++ cvmaker_back/cvmaker_back/settings/prod.py
@@ -9,7 +9,7 @@ from .base import *
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
